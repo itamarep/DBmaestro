d781972303e812ced60b916aaf8473a0e422b110
itamarep <Tpayhhit3#>
2021-03-04 13:36:29 +0200

    Test

--- DEV/Dev_Env_1/repo1/dbo/Table/Table_2/Script.sql
+++ DEV/Dev_Env_1/repo1/dbo/Table/Table_2/Script.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Table_2] (
     [c1] NCHAR (10) NULL,
-    [c2] NCHAR (10) NULL
+    [c2] NCHAR (10) NULL,
+    [c3] NCHAR (10) NULL
 );
 
 
---
+++ DEV/Dev_Env_1/repo1/dbo/Table/Table_6/Script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Table_6] (
+    [c1] NCHAR (10) NULL
+);
+
+
+GO
+
