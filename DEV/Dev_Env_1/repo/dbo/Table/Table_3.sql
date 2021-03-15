c0f4c1e8fc312a50ba188f0390a2c503ee75df71
itamaredbmaestro <Tpayhhit3#>
2021-03-15 11:32:15 +0200

    test 113

--- DEV/Dev_Env_1/repo/dbo/Table/Table_3.sql
+++ DEV/Dev_Env_1/repo/dbo/Table/Table_3.sql
@@ -1,7 +1,9 @@
 CREATE TABLE [dbo].[Table_3] (
-    [c1] NCHAR (10) NULL,
-    [c2] NCHAR (10) NULL,
-    [c3] NCHAR (10) NULL
+    [c1]  NCHAR (10) NULL,
+    [c2]  NCHAR (10) NULL,
+    [c3]  NCHAR (10) NULL,
+    [c20] NCHAR (10) NULL,
+    [c21] NCHAR (10) NULL
 );
 
 
