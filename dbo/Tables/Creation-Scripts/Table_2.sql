79aaa8eefd974e721b85a0c99db3903bba69cbdc
itamaredbmaestro <Tpayhhit3#>
2021-07-13 13:34:47 +0000

    add objects

---
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Table_2] (
+    [c20] NCHAR (10) NULL,
+    [c21] NCHAR (10) NULL,
+    [c22] NCHAR (10) NULL
+);
+
+
+GO
+
---
+++ dbo/Tables/Creation-Scripts/Table_4.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Table_4] (
+    [c41] NCHAR (10) NULL,
+    [c42] NCHAR (10) NULL,
+    [c43] NCHAR (10) NULL
+);
+
+
+GO
+
