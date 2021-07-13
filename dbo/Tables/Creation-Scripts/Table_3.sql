783a8c5936398a02231b75c645a93c20c366b7d8
itamarep <itamarep@gmail.com>
2021-07-13 17:23:37 +0300

    Delete dbo directory

--- dbo/Tables/Creation-Scripts/Table_3.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Table_3] (
-    [c31] NCHAR (10) NULL,
-    [c32] NCHAR (10) NULL,
-    [c33] NCHAR (10) NULL
-);
-
-
-GO
-
--- dbo/Tables/Creation-Scripts/Table_4.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Table_4] (
-    [c41] NCHAR (10) NULL,
-    [c42] NCHAR (10) NULL,
-    [c43] NCHAR (10) NULL
-);
-
-
-GO
-
